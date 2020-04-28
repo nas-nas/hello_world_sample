739632ab1d6ab34546223e7f14449029b06ef428
nas-nas <hakeemthedream1994@gmail.com>
2020-04-28 19:01:03 +0900

    make hello_world_sample app

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User
+  def initialize
+    @first_name = "Tohru"
+    @last_name = "Nagayasu"
+    @birthday = "1986/4/20"
+    @age = 34
+    @birthplace = "Tochigi/Utsunomiya"
+    @hobby = "video game"
+  end
+
+  def introduce
+    <<~EOS
+
+    私の名前は#{@first_name} #{@last_name}です。
+    誕生日は#{@birthday}で、年齢は#{@age}歳。
+    出身地は#{@birthplace}で、趣味は#{@hobby}です。
+
+    EOS
+  end
+end
